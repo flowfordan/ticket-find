5d7200c7b8b91130cbe159bf44bea507c3ee1ea2
flowfordan <flowfordan@gmail.com>
2022-05-28 14:21:52 +0300

    filter component

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -4,12 +4,63 @@ import { Card } from '../Card/Card';
 const Filter = () => {
 
     return(
-        <div className={styles.filter}>
-            <Card>
-                Filter
+        <div>
+            <Card >
+                <div className={styles.filter}>
+                <div>
+                    <div className={styles.h1}>Сортировать</div>
+                    <div>
+                        <input type="radio" id="radioButton" name="contact" value="phone"/>
+                        <label>fdfdfd</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="radioButton" name="contact" value="phone"/>
+                        <label>fdfdfd</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="radioButton" name="contact" value="phone"/>
+                        <label>fdfdfd</label>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.h1}>Фильтровать</div>
+                    <div>
+                        <div>
+                          <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter"/>
+                            <label>Subscribe to newsletter?</label>  
+                        </div>
+                        
+                        
+                        <div>
+                            <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter"/>
+                            <label>Subscribe to newsletter?</label>  
+                        </div>
+                        
+                    </div>
+
+                    <div className={styles.h2}>Цена</div>
+                    <div>
+                        От:
+                        <input type="number" placeholder='0'/>
+                    </div>
+                    <div>
+                        До:
+                        <input type="number" placeholder='100000'/>
+                    </div>
+
+                    <div className={styles.h2}>Авиакомпании</div>
+                    <ul>
+                        <li>
+                            <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter"/>Name
+                        </li>
+                    </ul>
+                </div>
+                </div>
             </Card>
-            
         </div>
+            
+        
     )
 }
 
